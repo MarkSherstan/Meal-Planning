a4dcdb785a7009cb7a1177f95a1a89a173992041
Mark Sherstan <mshersta@ualberta.ca>
2019-01-21 17:52:00 -0700

    Initial upload of Xcode workspace

---
+++ MenuPlanner+/MenuPlanner+/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuPlanner+
+//
+//  Created by Mark Sherstan on 2019-01-21.
+//  Copyright © 2019 Mark Sherstan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
