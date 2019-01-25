0a26d04cfaafd3851aa93aad59f4d49de709e557
Mark Sherstan <mshersta@ualberta.ca>
2019-01-24 17:56:37 -0700

    Begin to program GUI interactions and fill in labels and pop up buttons.  Tested one button.

--- MenuPlanner+/MenuPlanner+/ViewController.swift
+++ MenuPlanner+/MenuPlanner+/ViewController.swift
@@ -9,19 +9,53 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    // Load in Meals class
+    var meals = Meals()
+    
+    // Monday
+    @IBOutlet weak var mondayMeal: NSPopUpButton!
+    @IBOutlet weak var mondaySide: NSPopUpButton!
+    @IBOutlet weak var mondayMealPast1: NSTextField!
+    @IBOutlet weak var mondayMealPast2: NSTextField!
+    @IBOutlet weak var mondayMealPast3: NSTextField!
+    @IBOutlet weak var mondayCheckBox: NSButton!
+    
+    // Do any additional setup after loading the view
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        // Get the mains and sides
+        let mains = meals.getMains()
+        let sides = meals.getSides()
 
+        // Setup the pop up buttons
+        mondayMeal.removeAllItems()
+        mondayMeal.addItems(withTitles: mains)
+        mondayMeal.selectItem(at: 0)
+        
+        mondaySide.removeAllItems()
+        mondaySide.addItems(withTitles: sides)
+        mondaySide.selectItem(at: 0)
+    }
+    
+    // Update the view, if already loaded
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
         }
     }
 
+    // 
+    @IBAction func randomizeMealsButton(_ sender: Any) {
+    
+        if mondayCheckBox.state == .on {
+            mondayMealPast1.stringValue = "Button On"
+        } else {
+            mondayMealPast1.stringValue = "Button Off"
+        }
+        mondayCheckBox.state = .off
+    }
 
+    
 }
 
